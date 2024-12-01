a7e50742acc0a7a33109eda483378cf96302f6cd
Akshat B Gupta <gakshatb@gmail.com>
2024-12-01 19:09:56 +0530

    updated config file

--- config.py
+++ config.py
@@ -6,3 +6,15 @@ class dataconfig:
     database = 'database.db'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class admin:
+    admin_username = 'gakshatb'
+    admin_password = 'gakshatb'
+    admin_email = 'gakshatb@gmail.com'
+    admin_name = 'Akshat B Gupta'
+    admin_phone = '6397906947'
+    admin_address = 'B-123, Sector-12, Noida'
+    admin_pincode = '201301'
+    admin_state = 'Uttar Pradesh'
+    admin_country = 'India'
+    
