960f6caa80edad768535bbccfed9fa897a6cdcc3
Akshat B Gupta <gakshatb@gmail.com>
2024-12-01 18:59:35 +0530

    Updated model files

--- model.py
+++ model.py
@@ -1,11 +1,17 @@
 import sqlite3
 
+
+from config import dataconfig as data
+
+
 ############################################### Database setup ###################################################
+
 # Creating database
-con=sqlite3.connect("database.db")
+con=sqlite3.connect(data.database)
+
 
 # Initializing tables
-con=sqlite3.connect("database.db")
+con=sqlite3.connect(data.database)
 con.execute("create table if not exists user(pid integer primary key,name text,address text,contact text,pwd text)")
 con.execute("create table if not exists song(sid integer primary key,title text,singer text,date text,lyrics text,duration number, genre text, uploader text)")
 con.execute("create table if not exists album(playlist_name text, sid text, user text)")
@@ -13,9 +19,10 @@ con.execute("create table if not exists rating (user_name text,sid integer refer
 con.execute("create table if not exists creator(pid integer primary key autoincrement,name text)")
 con.close()
 
+
 # DBConnection
 def get_db_connection():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect(data.database)
     conn.row_factory = sqlite3.Row
     return conn
 ##################################################################################################################
